ce52a1f337824f22a310bc0af512160597124a86
Omar Shash <omarshash284@gmail.com>
2021-08-31 00:42:36 +0200

    First Commit
    First Commit

---
+++ medical_project/config.php
@@ -0,0 +1,19 @@
+<?php 
+session_start();
+
+// base file link 
+define("BL",__DIR__.'/');
+define("BLA",__DIR__.'/admin/');
+
+// base url
+define("BU","http://localhost/medicalProject/");
+define("BUA","http://localhost/medicalProject/admin/");
+define("ASSETS","http://localhost/medicalProject/assets/");
+
+require BL.'functions/db.php'; 
+
+
+
+
+?>
+
